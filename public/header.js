32a6a58cdff80775fd9dc173c9d8d4ae524362d6
Mutter-Liam <liammutter100@gmail.com>
2022-11-12 15:45:00 -0500

    bfff

--- public/header.js
+++ public/header.js
@@ -1,19 +1,15 @@
-document.getElementById("nav").innerHTML = `<div class="topnav">
-<a class="active" href="/index">Home</a>
-<a href="/account">My Profile</a>
-<a href="#contact">Stuff</a>
-</div>`;
+
 
 
 fetch("/get-account-data",{
     method: "POST",
 }).then((response)=>{
-    console.log(response)
+
     if(response.status == 200){
         return response.json()
     }
 }).then(data => {
-    console.log(data);
+    let name = data["name"];
     if (data["is_admin"]){
         document.getElementById("nav").innerHTML = `
         <div class="topnav">
@@ -21,6 +17,18 @@ fetch("/get-account-data",{
             <a href="/account">My Profile</a>
             <a href="#contact">Stuff</a>
             <a href="/admin">Admin</a>
+
+            <a href="#signout" id="signout">Sign Out</a>
+            <a id="signout">${name}</a>
+        </div>`;
+    }
+    else{
+        document.getElementById("nav").innerHTML = `<div class="topnav">
+            <a class="active" href="/index">Home</a>
+            <a href="#contact">Stuff</a>
+
+            <a href="#signout" id="signout">Sign Out</a>
+            <a href="/account" id="name">${name}</a>
         </div>`;
     }
 })
