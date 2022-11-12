cc7bbebf25c9472bc3aaa2c25b2936292a94a341
Mutter-Liam <liammutter100@gmail.com>
2022-11-12 02:25:38 -0500

    Wrote check-login

--- public/check-login.js
+++ public/check-login.js
@@ -1,3 +1,12 @@
-fetch("/get-account-data", (req, res) => {
-
-})
+fetch("/get-account-data",{
+    method: "POST",
+}).then((response)=>{
+    if (response.status == 409){
+        window.location = "/login";
+        return;
+    }
+    return response.json();
+}).then((data) => {
+    sessionStorage.setItem("user-data", data);
+    
+}).catch((r) => window.location = "/login");
