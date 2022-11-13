86ef2428ff2c77f05cd1ec941fabdc185d872869
NicoWasHere <nicolaesanderberg@gmail.com>
2022-11-12 21:14:46 -0500

    bubub

--- public/index.js
+++ public/index.js
@@ -1,3 +1,17 @@
-fetch("/get-announcements", {method: "POST"}).then((data) =>{
-    
+const feed = document.querySelector("#container")
+fetch("/get-posts", {method: "POST"}).then((data) =>{
+    return data.json()
+}).then((res)=>{
+    for(let i = 0;i<res.length;i++){
+        let post = res[i]
+        feed.innerHTML += `<div class = "announcement">
+        <div class = "announcementHeader">
+            <div class = "poster">${post.poster}</div><div class = "date">${post.date}</div>
+        </div>
+        <div class = "message">
+        ${post.message}
+        </div>
+        ${(post.image=="none")?"":`<img src = "${post.image}"/>`}
+    </div>`
+    }
 });
